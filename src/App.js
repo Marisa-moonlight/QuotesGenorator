b2bbabee6a0d319b95c333cb475c90b0b97177f7
Marisa-moonlight <93623155+Marisa-moonlight@users.noreply.github.com>
2022-11-02 15:28:05 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from'react';
+import Quotes from './quotes';
+import './App.css';
+
+function App () {
+return(
+  <div className='App'>
+    <h1>Quotes</h1>
+    <Quotes />
+  </div>
+  );
+}
+
+export default App;
